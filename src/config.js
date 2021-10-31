f8813301f9a2d07367b6f1f7bd36eb524ffd409d
Tony Nghieu <tnghieu@gmail.com>
2021-10-30 20:12:54 -0700

    Update API endpoints

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ const { REACT_APP_BACKEND_PORT } = process.env;
 const API_URL = `/.netlify/functions/app`;
 const LOCAL_BASE_URL = `http://localhost:${REACT_APP_BACKEND_PORT}${API_URL}`;
 const SERVER_BASE_URL = `https://mbi-server.netlify.app${API_URL}`;
-const VERIFY_MBI_URL = `${LOCAL_BASE_URL}/verify`;
-const GENERATE_MBI_URL = `${LOCAL_BASE_URL}/generate`;
+const VERIFY_MBI_URL = `${REACT_APP_BACKEND_PORT === 3001 ? LOCAL_BASE_URL : SERVER_BASE_URL}/verify`;
+const GENERATE_MBI_URL = `${REACT_APP_BACKEND_PORT === 3001 ? LOCAL_BASE_URL : SERVER_BASE_URL}/generate`;
 
 export {
     VERIFY_MBI_URL,
